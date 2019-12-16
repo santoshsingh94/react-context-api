1db5a6fc5673ff9fa85d6b77c88bbfc7bbc1591b
santoshsingh-94 <ausantoshsingh@gmail.com>
2019-12-16 15:49:44 +0530

    Basic Context API in React

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default Navbar;
